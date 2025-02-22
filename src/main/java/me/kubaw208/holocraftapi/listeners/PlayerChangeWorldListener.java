bdf891bb7c0347faa6512879b903ebaddf367851
kubaw208git <kubaw208kubaw208@gmail.com>
2025-02-22 14:49:32 +0100

    - add removing entity on world change. It's required if server uses plugin like 'SeamlessWorldChanger' that lets teleport players between worlds without animation. Normally if client changes world (with animation), client will automatically remove holograms. But if that animation of changing world will be skipped, client won't clear holograms (so in new world he will see hologram from previous world).

--- src/main/java/me/kubaw208/holocraftapi/listeners/PlayerChangeWorldListener.java
+++ src/main/java/me/kubaw208/holocraftapi/listeners/PlayerChangeWorldListener.java
@@ -1,15 +1,21 @@
 package me.kubaw208.holocraftapi.listeners;
 
-import me.kubaw208.holocraftapi.structs.Hologram;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
 import me.kubaw208.holocraftapi.api.HologramManager;
+import me.kubaw208.holocraftapi.packets.RemoveEntitiesPacket;
+import me.kubaw208.holocraftapi.structs.Hologram;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 
+import java.util.List;
+
 public class PlayerChangeWorldListener implements Listener {
 
     private final HologramManager hologramManager = HologramManager.getInstance();
+    private final ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
 
     @EventHandler
     public void onChangeWorld(PlayerChangedWorldEvent e) {
@@ -18,6 +24,8 @@ public class PlayerChangeWorldListener implements Listener {
         for(Hologram hologram : hologramManager.getHolograms()) {
             if(hologram.canSee(player) && hologram.getLocation().getWorld().equals(player.getWorld())) {
                 hologram.showHologram(player, true);
+            } else {
+                protocolManager.sendServerPacket(player, new RemoveEntitiesPacket(List.of(hologram.getEntityID())));
             }
         }
     }
